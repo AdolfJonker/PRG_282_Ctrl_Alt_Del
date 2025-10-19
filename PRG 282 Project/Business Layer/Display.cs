837cbf4e7271b0b866fc36dc0beb90673b86f779
ElckeCode <vanderbergelcke101@gmail.com>
2025-10-19 15:24:51 +0200

    Inserted necessary using system info...

--- PRG 282 Project/Business Layer/Display.cs
+++ PRG 282 Project/Business Layer/Display.cs
@@ -12,37 +12,37 @@ namespace PRG_282_Project.Classes
     {
          private string filePath = "superheroes.txt";
 
- //METHOD: Reads superheroes from the textfile and returns it as a list
- //Any UI work (DataGridView) is done in the Form Class
- public List<string[]> LoadSuperheroes()
- {
-     List<string[]> heroes = new List<string[]>();
-
-     try
-     {
-         //Check if the file exists and then read it line by line
-         if (File.Exists(filePath))
+         //METHOD: Reads superheroes from the textfile and returns it as a list
+         //Any UI work (DataGridView) is done in the Form Class
+         public List<string[]> LoadSuperheroes()
          {
-             using (StreamReader sr = new StreamReader(filePath))
+             List<string[]> heroes = new List<string[]>();
+
+             try
              {
-                 string line;
-                 while ((line = sr.ReadLine()) != null)
+                 //Check if the file exists and then read it line by line
+                 if (File.Exists(filePath))
                  {
-                     string[] parts = line.Split(',');
-                     heroes.Add(parts);
+                     using (StreamReader sr = new StreamReader(filePath))
+                     {
+                         string line;
+                         while ((line = sr.ReadLine()) != null)
+                         {
+                             string[] parts = line.Split(',');
+                             heroes.Add(parts);
+                         }
+                     }
+                 }
+                 else
+                 {
+                     Console.WriteLine("File not found:" + filePath);
                  }
              }
+             catch (Exception ex)
+             {
+                 Console.WriteLine(ex.Message);
+             }
+             return heroes;
          }
-         else
-         {
-             Console.WriteLine("File not found:" + filePath);
-         }
-     }
-     catch (Exception ex)
-     {
-         Console.WriteLine(ex.Message);
-     }
-     return heroes;
- }
     }
 }
--- PRG 282 Project/Presentation Layer/DisplayInfo.cs
+++ PRG 282 Project/Presentation Layer/DisplayInfo.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PRG_282_Project.Business_Layer;
+using PRG_282_Project.Data_Layer;
 
 namespace PRG_282_Project.Presentation_Layer
 {
