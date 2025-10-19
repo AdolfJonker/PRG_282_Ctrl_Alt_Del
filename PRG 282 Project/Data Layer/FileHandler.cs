cd26098ab8e69239c34f76df0d66a3b0586a567d
Kaylee2809 <kaylee.hiralall@gmail.com>
2025-10-19 13:22:41 +0200

    Implement file read and write methods in FileHandler
    Added methods to read from 'superheroes.txt' and write to 'summary.txt'.

--- PRG 282 Project/Data Layer/FileHandler.cs
+++ PRG 282 Project/Data Layer/FileHandler.cs
@@ -8,5 +8,29 @@ namespace PRG_282_Project.Data_Layer
 {
     internal class FileHandler
     {
+         public List<string> ReadFiles()
+ {
+     List<string> fileLines = new List<string>();
+
+     fileLines = File.ReadAllLines("superheroes.txt").ToList();
+
+     return fileLines;
+ }
+
+ public List<string> WriteToFile()
+ {
+     List<string> output = new List<string>();
+
+     FormatHandler handler = new FormatHandler();
+
+     foreach (var p in handler.FormatData())
+     {
+         output.Add(p.ToString());
+     }
+
+     File.WriteAllLines("summary.txt", output);
+
+     return output;
+ }
     }
 }
