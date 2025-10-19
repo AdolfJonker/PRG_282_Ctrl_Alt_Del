a1f27d74ca9508e5c01962ce9c2cc435e2312acf
Kaylee2809 <kaylee.hiralall@gmail.com>
2025-10-19 13:21:17 +0200

    Add FormatData method to FormatHandler class

--- PRG 282 Project/Business Layer/FormatHandler.cs
+++ PRG 282 Project/Business Layer/FormatHandler.cs
@@ -8,5 +8,19 @@ namespace PRG_282_Project.Business_Layer
 {
     internal class FormatHandler
     {
+        public List<Superhero> FormatData()
+{
+    List<Superhero> hero = new List<Superhero>();
+
+    FileHandler handler = new FileHandler();
+    foreach (var item in handler.ReadFiles())
+    {
+        string[] p = item.Split(',');
+
+        hero.Add(new Superhero(p[0], p[1], p[2], p[3], p[4]));
+    }
+
+    return hero;
+}
     }
 }
